3c30b78269691721e0ca9e6eae6821485965bcd8
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-22 15:50:27 -0500

    Primer Commit

---
+++ src/main/java/com/nttdata/repository/IDepositRepository.java
@@ -0,0 +1,7 @@
+package com.nttdata.repository;
+
+import com.nttdata.model.Deposit;
+
+public interface IDepositRepository extends IRepository<Deposit, String> {
+
+}
---
+++ src/main/java/com/nttdata/service/IDepositService.java
@@ -0,0 +1,7 @@
+package com.nttdata.service;
+
+import com.nttdata.model.Deposit;
+
+public interface IDepositService extends ICRUDService<Deposit, String> {
+	
+}
